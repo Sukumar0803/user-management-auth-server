3d273bfefef0413194f323878e150e074f7cbbcb
Sukumar0803 <kalpam.sukumar@gmail.com>
2025-01-31 22:28:36 +0530

    Initial commit - Auth Server

---
+++ src/main/java/com/ecommerce/usermanagement/UserManagementApplication.java
@@ -0,0 +1,29 @@
+package com.ecommerce.usermanagement;
+
+import com.ecommerce.usermanagement.repository.JpaRegisteredClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat;
+import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
+
+import java.time.Duration;
+import java.util.UUID;
+
+@SpringBootApplication
+public class UserManagementApplication {
+
+	@Autowired
+	private JpaRegisteredClientRepository registeredClientRepository;
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagementApplication.class, args);
+	}
+}
---
+++ src/main/java/com/ecommerce/usermanagement/repository/ClientRepository.java
@@ -0,0 +1,12 @@
+package com.ecommerce.usermanagement.repository;
+
+import com.ecommerce.usermanagement.entity.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, String> {
+    Optional<Client> findByClientId(String clientId);
+}
